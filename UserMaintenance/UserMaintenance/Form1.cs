b4e18bfc0d02d1a199dc2057e43c79965d8357fa
Májer Benjamin <benjamin.majer@exagent.group>
2021-10-03 10:24:35 +0200

    save button

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,6 +33,26 @@ namespace UserMaintenance
                 FullName = textBox1.Text,
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+                {
+                    foreach (User u in users)
+                    {
+                        sw.Write(u.ID);
+                        sw.Write(";");
+                        sw.Write(u.FullName);
+                        sw.Write(";");
+
+                    }
+                }
+            }
         }
     }
 }
