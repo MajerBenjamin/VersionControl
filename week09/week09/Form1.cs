31d4ffd8fd22bcfe94e493eb1348ad81d860a56f
Májer Benjamin <benjamin.majer@exagent.group>
2021-12-03 16:40:37 +0100

    initial steps

---
+++ week09/week09/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using week09.Entities;
+
+namespace week09
+{
+    public partial class Form1 : Form
+    {
+        List<Person> Population = new List<Person>();
+        List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public List<Person> GetPopulation(string csvpath)
+    {
+        List<Person> population = new List<Person>();
+
+        using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+        {
+            while (!sr.EndOfStream)
+            {
+                var line = sr.ReadLine().Split(';');
+                population.Add(new Person()
+                {
+                    BirthYear = int.Parse(line[0]),
+                    Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                    NbrOfChildren = int.Parse(line[2])
+                });
+            }
+        }
+
+        return population;
+    }
+}
